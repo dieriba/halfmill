7687ad3c7fcf82217ade56d0ab317f15d07f6118
dieriba <t.dieriba@gmail.com>
2024-09-25 22:45:33 +0200

    added main futures

--- backend/halfmill-server/src/main.rs
+++ backend/halfmill-server/src/main.rs
@@ -1,6 +1,17 @@
-use tokio::net::TcpListener;
+use anyhow::Result;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-fn main() {
-    println!("Hello, world! wsh");
+async fn halfmill_main() {
+
+}
+
+fn main() -> Result<()> {
+    let tokio_runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(32)
+        .build()
+        .unwrap();
+    tokio_runtime.block_on(halfmill_main());
+    Ok(())
 }
